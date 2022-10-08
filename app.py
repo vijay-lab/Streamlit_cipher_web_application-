0603cd63a8d779dc61bafdf9455f4f2b3922445d
Tapan Vijay <tapanvijay@outlook.com>
2022-10-08 18:58:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import scikit_learn
 
-import pandas as pd
-from datetime import datetime
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn import metrics
-from sklearn.metrics import f1_score,precision_score,recall_score
-import pickle
 
 st.title("Encrypted Text Decryption App")
 cipher_text = st.text_input("Enter encrypted text", "")
