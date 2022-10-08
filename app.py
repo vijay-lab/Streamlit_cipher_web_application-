93f2ae2dd0a308dff9a48a1eade4f6d1c42d92b7
Tapan Vijay <tapanvijay@outlook.com>
2022-10-08 18:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("A Simple Streamlit Web App")
-name = st.text_input("Enter your name", ‘’)
+name = st.text_input("Enter your name", '')
 st.write(f'Hello {name}!")
 x = st.slider('Select an integer x', 0, 10, 1)
 y = st.slider('Select an integer y', 0, 10, 1)
