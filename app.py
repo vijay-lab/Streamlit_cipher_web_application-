e92d29c2f4ece5dae2261f50c6bbad73f9d98f4f
Tapan Vijay <tapanvijay@outlook.com>
2022-10-08 19:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,4 @@ loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
 test_vec = loaded_vectorizer.transform([cipher_text])
 cipher_class = loaded_model.predict(test_vec)
 
+st.write(cipher_class)
