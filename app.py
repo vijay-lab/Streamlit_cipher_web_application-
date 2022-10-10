6cdcacf83d5cedc4f45ec4ab2b8bf6544ff04886
Tapan Vijay <tapanvijay@outlook.com>
2022-10-10 17:55:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,39 @@ import sklearn
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
+# Preemptying Strealit default footer
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.linkedin.com/in/tapanvijay/" target="_blank">Tapan Vijayvergiya</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+
+
 st.title("Encrypted Text Decryption App")
 plain_text_df = pd.read_csv('test.csv')
 
